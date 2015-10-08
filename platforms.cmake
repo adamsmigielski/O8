2cfaef8108a3a5a67e09be4f2a2915989134cf35
adamsmigielski <adam.smigielski@gmail.com>
2015-10-08 09:05:23 +0200

    Reworking configuration

--- platforms.cmake
+++
@@ -1,9 +0,0 @@
-# Select target platform
-IF(WIN32)
-    SET(PLATFORM "win_32_64")
-ENDIF(WIN32)
-    
-
-# Include target-specific definitions
-INCLUDE_DIRECTORIES(src/Utilities/platforms/${PLATFORM})
-ADD_SUBDIRECTORY(src/Utilities/platforms/${PLATFORM})
